132f33c699314da4905acf56d3da1a11e9a57ee8
Tai Le <leanhtai253@gmail.com>
2023-07-14 21:44:15 +0700

    remove database sensitive info

--- src_demo/src/database/connector.py
+++ src_demo/src/database/connector.py
@@ -2,7 +2,7 @@ import psycopg2
 
 class DatabaseConnector:
     def __init__(self):
-        self.url = 'postgres://root:B6sFCAbNTvrgqvzna8FCXFMOHz2KIkid@dpg-ci6jam18g3nfucbk7b10-a.singapore-postgres.render.com/vahdb'
+        self.url = ''
 
     def connect(self):
         conn = psycopg2.connect(self.url)
